e2ef1aff10aca1e1c99f2069c4c3c11b392961d1
Emilio Scodeler <emilios@ciandt.com>
2022-06-23 21:13:56 -0300

    AWS Serverless sample projects

---
+++ client-front/src/containers/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Client Med</h1>
+        <p className="text-muted">Consultório Online</p>
+      </div>
+    </div>
+  );
+}
